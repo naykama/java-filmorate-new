3601c5de63a25414213be4ef6c1431fc7e78e86e
m.furtov <mail5692@yandex.com>
2023-11-29 18:12:01 +0300

    Чекстаил4

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserValidationTest.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.ConstraintViolation;
