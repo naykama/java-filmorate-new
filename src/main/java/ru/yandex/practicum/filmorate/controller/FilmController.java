2c6ce8f8618627045bf55aa24113557b8a96c755
m.furtov <mail5692@mail.ru>
2023-12-13 00:30:02 +0300

    работа над FilmService

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,11 +1,14 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.exeption.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
@@ -17,40 +20,50 @@ import java.util.List;
 public class FilmController {
 //    private final int maxDescriptionSize = 200;
 //    private final LocalDate minReleaseDate = LocalDate.of(1895, 12, 28);
-    private int id = 1;
-    private static final List<Film> films = new ArrayList<>();
+    private final InMemoryFilmStorage inMemoryFilmStorage;
+    private final FilmService filmService;
+
+    @Autowired
+    public FilmController(InMemoryFilmStorage inMemoryFilmStorage,FilmService filmService){
+        this.inMemoryFilmStorage=inMemoryFilmStorage;
+        this.filmService=filmService;
+    }
+
+//    private int id = 1;
+//    private static final List<Film> films = new ArrayList<>();
+
 
     @GetMapping("/films")
     public List<Film> findAll() {
-        log.info("Список фильмов выведен, сейчас их количество: " + films.size());
-        return films;
+//        log.info("Список фильмов выведен, сейчас их количество: " + films.size());
+        return inMemoryFilmStorage.findAll();
     }
 
     @PostMapping(value = "/films")
     public Film post(@Valid @RequestBody Film film) {
 //        validate(film);
-        film.setId(incrementId());
-        films.add(film);
+//        film.setId(incrementId());
+//        films.add(film);
         log.info(film.getName() + " был добавлен к списку филюмов");
-        return film;
+        return inMemoryFilmStorage.post(film);
     }
 
     @PutMapping(value = "/films")
     public Film put(@Valid @RequestBody Film film) {
 //        validate(film);
-        boolean filmIdExist = films.stream().allMatch(userFoeEach -> userFoeEach.getId() == film.getId());
-        if (!filmIdExist) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Фильм с указанным ID не найден");
-        }
-        films.removeIf(filmForEach -> filmForEach.getId() == film.getId());
-        films.add(film);
+//        boolean filmIdExist = films.stream().allMatch(userFoeEach -> userFoeEach.getId() == film.getId());
+//        if (!filmIdExist) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Фильм с указанным ID не найден");
+//        }
+//        films.removeIf(filmForEach -> filmForEach.getId() == film.getId());
+//        films.add(film);
         log.info("\"" + film.getId() + "\" фильм под данным id был обновлен");
-        return film;
+        return inMemoryFilmStorage.put(film);
     }
 
-    private int incrementId() {
-        return id++;
-    }
+//    private int incrementId() {
+//        return id++;
+//    }
 
 //    private void validate(Film film) {
 //        if (film.getName() == null || film.getName().isBlank()) {
