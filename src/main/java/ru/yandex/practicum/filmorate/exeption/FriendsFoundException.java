9593dc1ff718e027c53b46ea1b2d420fd7ce1f57
m.furtov <mail5692@mail.ru>
2023-12-14 21:12:15 +0300

    чексатил

--- src/main/java/ru/yandex/practicum/filmorate/exeption/FriendsFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/FriendsFoundException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exeption;
 
-public class FriendsFoundException extends RuntimeException{
-    public FriendsFoundException(String e){
+public class FriendsFoundException extends RuntimeException {
+    public FriendsFoundException(String e) {
         super(e);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/exeption/UserFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/UserFoundException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exeption;
 
 public class UserFoundException extends RuntimeException {
-    public UserFoundException(String s){
+    public UserFoundException(String s) {
         super(s);
     }
 }
