cd71651f13e7a0f1e62f394b9d8f2789413effb5
m.furtov <mail5692@yandex.com>
2023-12-19 14:21:58 +0300

    чекстаил

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -4,8 +4,6 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
-import java.util.Map;
-
 public interface UserStorage {
     List<User> findAll();
 
